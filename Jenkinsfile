40821e136948379335f2121fce362ec1831061c2
Destroyer616 <saikiran.reddy916@gmail.com>
2024-10-21 15:24:18 +0530

    added a jenkinsfile to build and push image with automatic versioning

---
+++ Jenkinsfile
@@ -0,0 +1,46 @@
+def gv
+
+pipeline {
+    agent any
+    stages {
+        stage("init") {
+            steps {
+                script {
+                    gv = load "script.groovy"
+                }
+            }
+        }
+        stage("increment") {
+            steps {
+                script {
+                    echo "increment version"
+                    gv.Increment()
+                }
+            }
+        }
+        stage("build jar") {
+            steps {
+                script {
+                    echo "building jar"
+                    gv.buildJar()
+                }
+            }
+        }
+        stage("build image") {
+            steps {
+                script {
+                    echo "building image"
+                    gv.buildImage()
+                }
+            }
+        }
+        stage("push image to repo") {
+            steps {
+                script {
+                    echo "push image to repo"
+                    gv.pushImage()
+                }
+            }
+        }
+    }   
+}
--- script.groovy
+++ script.groovy
@@ -1,19 +1,29 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
+def Increment() {
+    echo "Increment the version..."
+    sh 'mvn build-helper:parse-version versions:set \
+        -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalversion} \
+        versions:commit'
+    def match = readFile('pom.xml') =~ '<version>(.+)</version>'
+    def version = match[0][1]
+    env.IMAGE_NAME = "$version-$BUILD_NUMBER"
 } 
+
+def buildJar() {
+    sh 'mvn clean package'
+}
+
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+    withCredentials([usernamePassword(credentialsId: 'docker-hosted', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "echo $PASS | docker login -u $USER --password-stdin nexus.nexus.orb.local:8082"
+        sh "docker build -t nexus.nexus.orb.local:8082/jenkins-maven-app:$IMAGE_NAME ."
     }
 } 
 
-def deployApp() {
-    echo 'deploying the application...'
+def pushImage() {
+    echo 'push the image to docker hosted repo..'
+    sh "docker push nexus.nexus.orb.local:8082/jenkins-maven-app:$IMAGE_NAME"
 } 
 
 return this
